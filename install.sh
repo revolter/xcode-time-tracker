fa8c9665b766698351402bcc7c62f7f4482e8697
Iulian Onofrei <iulian.onofrei@yahoo.com>
2020-07-19 14:31:38 +0300

    Removed the old install script

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Creating if not exist
-[ -d ~/.timecheck ] || mkdir ~/.timecheck
-
-cp ./scripts/project_end.py ~/.timecheck/
-cp ./scripts/project_start.py ~/.timecheck/
-
-echo "Everything is ready. Now it's time to configure Xcode behaviors (see README.md)"
